0a281afecabdda7988d230217f73d9f7cc4e0a89
David Baynard <davidbaynard@gmail.com>
2018-09-03 01:14:31 +0100

    Add error chain

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! # Splitting and joining pdfs
+//!
+
+#[macro_use]
+extern crate error_chain;
+
+mod errors;
+use errors::*;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#![recursion_limit = "1024"]
+
+extern crate padfoot;
+use padfoot::*;
+
 fn main() {
     println!("Hello, world!");
 }
