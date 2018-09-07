58df82b778509853649b2a746dff11e755ad6503
David Baynard <davidbaynard@gmail.com>
2018-09-08 00:40:07 +0100

    Add error method for error-chain compatibility

--- src/errors.rs
+++ src/errors.rs
@@ -1,1 +1,19 @@
+use std::result;
+
 error_chain!{}
+
+pub trait ErrorChainable<T> {
+    fn error(self, e: impl Into<Error>) -> Result<T>;
+}
+
+impl<T> ErrorChainable<T> for Option<T> {
+    fn error(self, e: impl Into<Error>) -> Result<T> {
+        self.ok_or_else(|| e.into())
+    }
+}
+
+impl<T, E> ErrorChainable<T> for result::Result<T, E> {
+    fn error(self, e: impl Into<Error>) -> Result<T> {
+        self.map_err(|_| e.into())
+    }
+}
